ccbb19fc67a85c2e7c008cc82a6516656cda7371
picatrix1899 <FlorianZilkenat@gmx.de>
2019-01-31 17:50:25 +0100

    extended the geometry section.

--- barghos-math/src/org/barghos/math/api/vector/IQuatR.java
+++ barghos-math/src/org/barghos/math/api/vector/IQuatR.java
@@ -1,11 +1,12 @@
 package org.barghos.math.api.vector;
 
-import org.barghos.core.api.tuple.ITup4R;
+import org.barghos.core.api.tuple.ITup4dR;
+
 
 /**
- * This interface grants access to all reading methods of an 4-dimensional quaternion.
+ * This interface grants access to all reading methods of an 3-dimensional quaternion.
  */
-public interface IQuatR extends ITup4R
+public interface IQuatR extends ITup4dR
 {
 	
 }
--- barghos-math/src/org/barghos/math/geometry/Ray3f.java
+++ barghos-math/src/org/barghos/math/geometry/Ray3f.java
@@ -1,6 +1,6 @@
 package org.barghos.math.geometry;
 
-import org.barghos.core.api.tuple.ITup3fR;
+import org.barghos.core.api.tuple.ITup3R;
 import org.barghos.math.vector.Vec3f;
 
 public class Ray3f
@@ -8,11 +8,11 @@ public class Ray3f
 	public final Vec3f d = new Vec3f();
 	
 	public Ray3f() { }
-	public Ray3f(ITup3fR t) { assert(t != null); set(t); }
+	public Ray3f(ITup3R t) { assert(t != null); set(t); }
 	public Ray3f(Ray3f r) { assert(r != null); set(r); }
 	public Ray3f(double x, double y, double z) { set(x, y, z); }
 	
-	public Ray3f set(ITup3fR t) { assert(t != null); this.d.set(t); return this; }
+	public Ray3f set(ITup3R t) { assert(t != null); this.d.set(t); return this; }
 	public Ray3f set(Ray3f r) { this.d.set(r.d); return this; }
 	public Ray3f set(double x, double y, double z) { this.d.set(x, y, z); return this; }
 	
