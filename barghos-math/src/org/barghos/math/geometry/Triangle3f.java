8e45465c26ac7b4faca71ac313af07c5469fe370
picatrix1899 <FlorianZilkenat@gmx.de>
2019-03-28 17:55:55 +0100

    Improved Memory-Allocation of Line3f

---
+++ barghos-math/src/org/barghos/math/geometry/Triangle3f.java
@@ -0,0 +1,60 @@
+package org.barghos.math.geometry;
+
+import org.barghos.core.api.tuple.ITup3R;
+
+import org.barghos.math.point.Point3f;
+
+public class Triangle3f
+{
+	public final Point3f p1 = new Point3f();
+	public final Point3f p2 = new Point3f();
+	public final Point3f p3 = new Point3f();
+	
+	public Triangle3f() { }
+	public Triangle3f(Triangle3f t)
+	{
+		
+	}
+	
+	public Triangle3f(ITup3R p1, ITup3R p2, ITup3R p3)
+	{
+		
+	}
+
+	public Triangle3f(double x1, double y1, double z12, ITup3R p2, ITup3R p3)
+	{
+		
+	}
+	
+	public Triangle3f(ITup3R p1, double x2, double y2, double z2, ITup3R p3)
+	{
+		
+	}
+	
+	public Triangle3f(ITup3R p1, ITup3R p2, double x3, double y3, double z3)
+	{
+		
+	}
+	
+	public Triangle3f(double x1, double y1, double z1, double x2, double y2, double z2, ITup3R p3)
+	{
+		
+	}
+	
+	public Triangle3f(double x1, double y1, double z1, ITup3R p2, double x3, double y3, double z3)
+	{
+		
+	}
+	
+	public Triangle3f(ITup3R p1, double x2, double y2, double z2, double x3, double y3, double z3)
+	{
+		
+	}
+	
+	public Triangle3f(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3)
+	{
+		
+	}
+	
+	
+}
