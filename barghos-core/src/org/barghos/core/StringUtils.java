f6ff02a7aab710dd94f8415353d9937c8cc93b5a
picatrix1899 <FlorianZilkenat@gmx.de>
2019-05-08 23:09:11 +0200

    .

--- barghos-core/src/org/barghos/core/StringUtils.java
+++ barghos-core/src/org/barghos/core/StringUtils.java
@@ -22,4 +22,20 @@ public class StringUtils
 		
 		return str;
 	}
+	
+	public static String repeat(String str, int n)
+	{
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < n; i++)
+			builder.append(str);
+		
+		return builder.toString();
+	}
+	
+	public static StringBuilder repeat(String str, int n, StringBuilder builder)
+	{
+		for(int i = 0; i < n; i++)
+			builder.append(str);
+		return builder;
+	}
 }
--- barghos-core/src/org/barghos/core/profiler/CascadedProfile.java
+++ barghos-core/src/org/barghos/core/profiler/CascadedProfile.java
@@ -3,8 +3,24 @@ package org.barghos.core.profiler;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.barghos.core.StringUtils;
+
 public class CascadedProfile
 {
 	public Map<String,CascadedProfile> times = new HashMap<>();
-	public long averageDuration;
+	public double averageDuration;
+	public String name;
+	
+	public void dump(StringBuilder builder, int level)
+	{
+		if(times.isEmpty())
+		{
+			StringUtils.repeat("\t", level, builder);
+			builder.append(this.name);
+			builder.append("\t");
+			builder.append(this.averageDuration / 1000l).append("µs");
+			builder.append("\t");
+			builder.append(this.averageDuration / 1000000l).append("ms");
+		}
+	}
 }
